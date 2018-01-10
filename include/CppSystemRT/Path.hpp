05375193989610e702f7758b5a191b698297610b
Mauro Joel Schütz <mjschutz@outlook.com>
2018-01-10 09:34:15 +0000

    Change SharedObject Path to prefix it with 'lib'

--- include/CppSystemRT/Path.hpp
+++ include/CppSystemRT/Path.hpp
@@ -18,7 +18,19 @@ constexpr const char*	SOBJ_EXT	= ".so";
 std::string Var(std::string const& name, std::string const& value = std::string());
 std::string	Parse(std::string const& path);
 inline std::string	Exec(std::string const& path) { return Parse(path+EXE_EXT); }
-inline std::string	SharedObject(std::string const& path) { return Parse(path+SOBJ_EXT); }
+inline std::string	SharedObject(std::string const& path) {
+#ifdef _WIN32
+	return Parse(path+SOBJ_EXT);
+#else
+	std::size_t found = path.find_last_of("/");
+
+	if (found == std::string::npos) {
+		return "lib" + path;
+	}
+
+	return Parse(path.substr(0, found) + "/lib" + path.substr(found+1)+SOBJ_EXT);
+#endif
+}
 
 }
 
